4fc635940b563ff3ef8d571b5dabeff1216efec4
cat_in_136 <cat.in.136+github@gmail.com>
2020-04-01 00:45:18 +0900

    introduce badge-device open codes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,75 @@
+extern crate hidapi;
+
+use std::fmt;
+use std::fmt::Debug;
+use std::process::exit;
+
+use hidapi::{HidApi, HidDevice, HidError};
+
+/// Vendor ID of the LED Badge
+const BADGE_VID: u16 = 0x0416;
+/// Product ID of the LED Badge
+const BADGE_PID: u16 = 0x5020;
+
+#[derive(Debug)]
+pub enum BadgeError {
+    /// Badge Not Found i.e. the LED Badge is not connected to the PC.
+    BadgeNotFound,
+    /// IO Error.
+    Io(HidError),
+}
+
+impl fmt::Display for BadgeError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use BadgeError::*;
+
+        match self {
+            BadgeNotFound => f.write_str("Badge Not Found"),
+            Io(_error) => f.write_str("IO Error"),
+        }
+    }
+}
+
+impl From<HidError> for BadgeError {
+    fn from(e: HidError) -> Self {
+        BadgeError::Io(e)
+    }
+}
+
+pub struct Badge {
+    device: HidDevice,
+}
+
+impl Badge {
+    fn new() -> Result<Self, BadgeError> {
+        let api = HidApi::new()?;
+
+        let num_badge = api
+            .device_list()
+            .filter(|info| info.vendor_id() == BADGE_VID && info.product_id() == BADGE_PID)
+            .count();
+        if num_badge == 0 {
+            return Err(BadgeError::BadgeNotFound);
+        } else if num_badge > 1 {
+            return Err(BadgeError::BadgeNotFound); // TODO change error messager.
+        }
+
+        let device = api.open(BADGE_VID, BADGE_PID)?;
+
+        Ok(Badge { device })
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _badge = match Badge::new() {
+        Ok(badge) => badge,
+        Err(err) => {
+            eprintln!("Error: {}", err);
+            exit(1);
+            return;
+        }
+    };
+
+    println!("Badge Found!");
+    exit(0);
 }
